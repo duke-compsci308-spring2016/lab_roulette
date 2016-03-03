a5cec75a053c7bb069125abb3b7b3042eda49f65
torrescar <cmt57@duke.edu>
2016-03-03 16:14:24 -0500

    Step 2 of Roulette

--- src/roulette/Factory.java
+++ src/roulette/Factory.java
@@ -7,23 +7,28 @@ import java.util.List;
 import util.ConsoleReader;
 
 public class Factory {
-    private Bet[] myPossibleBets = { 
-            new RedBlackBet("Red or Black", 1),
-            new OddEven("Odd or Even", 1),
-            new Consecutive("Three in a Row", 11),
-        };
+    
+    private String[] myPossibleBets = {"Red or Black", "Odd or Even", "Three in a row"};
 	
     private Bet promptForBet () {
         System.out.println("You can make one of the following types of bets:");
         for (int k = 0; k < myPossibleBets.length; k++) {
-            System.out.println(String.format("%d) %s", (k + 1), myPossibleBets[k].getDescription()));
+            System.out.println(String.format("%d) %s", (k + 1), myPossibleBets[k]));
         }
-        return myPossibleBets[ConsoleReader.promptRange("Please make a choice", 1, myPossibleBets.length) - 1];
+        Object instance = null;
+		try {
+			instance = Class.forName(myPossibleBets[ConsoleReader.promptRange("Please make a choice", 1, myPossibleBets.length) - 1]).newInstance();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        return (Bet) instance;
     }
 
-	public static void main (String[] args) {
-		
-		
-		
-	}
 }
