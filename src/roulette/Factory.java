06f0c8871fc0b6ca22e6dc632d62ee0ba6b1e6c4
Alan Cavalcanti <alankmc@gmail.com>
2016-03-03 17:12:54 -0500

    Third Part: Added in Properties

--- src/roulette/Factory.java
+++ src/roulette/Factory.java
@@ -1,30 +1,42 @@
 package roulette;
 
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
+import java.util.Properties;
 
 public class Factory 
 {
 	private ArrayList<Bet> typesOfBets;
-
-	private enum PossibleBets
-	{
-		RED_BLACK, ODD_EVEN, THREE_CONSECUTIVES
-	}
-
+	private Properties betProperties;
+	
 	public Factory()
 	{
 		typesOfBets = new ArrayList<Bet>();
+		
+		betProperties = new Properties();
+		
+		try {
+			betProperties.load(getClass().getClassLoader().getResourceAsStream("betProperties.properties"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
 
-		addBet(RedBlack.class, "Red or Black", 1);
-		addBet(OddEven.class, "Odd or Even", 1);
-		addBet(ThreeConsecutive.class, "3 consecutive",11);
+		addBet(RedBlack.class, betProperties );
+		addBet(OddEven.class, betProperties);
+		addBet(ThreeConsecutive.class, betProperties);
 	}
 
-	private void addBet( Class betType, String description, int odds )
-	{
+	private void addBet( Class betType, Properties betProperties)
+	{		
+		String[] properties = betProperties.getProperty(RedBlack.class.getName()).split(",");
+		
 		try {
-			typesOfBets.add((Bet) betType.getConstructor(String.class, int.class).newInstance(description, odds));
+			typesOfBets.add((Bet) betType.
+					getConstructor(String.class, int.class).
+					newInstance(properties[0], Integer.parseInt(properties[1])));
 		} catch (InstantiationException | IllegalAccessException
 				| IllegalArgumentException | InvocationTargetException
 				| NoSuchMethodException | SecurityException e) {
