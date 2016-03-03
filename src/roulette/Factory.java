aa30784f1b94e7cfce89223abcff0f2b93c6ab96
Andy <ownz.andy@gmail.com>
2016-03-03 16:13:31 -0500

    added reflection

--- src/roulette/Factory.java
+++ src/roulette/Factory.java
@@ -3,19 +3,40 @@ package roulette;
 import util.ConsoleReader;
 
 public class Factory {
+
+    private String[] betTypes = {
+                                   "RedBlack",
+                                   "OddEven",
+                                   "ThreeConsecutive"
+    };
     
-    private Bet[] myPossibleBets = {
-                                    new RedBlack("Red or Black", 1),
-                                    new OddEven("Odd or Even", 1),
-                                    new ThreeConsecutive("Three in a Row", 11),
-                                };
+    private String[] betDescriptions = {
+                                 "Red or Black",
+                                 "Odd or Even",
+                                 "Three in a Row"
+  };
     
+    private int[] betOdds = {
+                             1,
+                             1,
+                             11
+         };
+            
     public Bet promptForBet() {
         System.out.println("You can make one of the following types of bets:");
-        for (int k = 0; k < myPossibleBets.length; k++) {
-            System.out.println(String.format("%d) %s", (k + 1), myPossibleBets[k].getDescription()));
+        for (int k = 0; k < betTypes.length; k++) {
+            System.out.println(String.format("%d) %s", (k + 1), betDescriptions[k]));
         }
-        int response = ConsoleReader.promptRange("Please make a choice", 1, myPossibleBets.length);
-        return myPossibleBets[response - 1];
+        int response = ConsoleReader.promptRange("Please make a choice", 1, betTypes.length);
+        Bet bet = null;
+        try {
+            bet = (Bet) Class.forName("roulette." + betTypes[response - 1])
+                    .getConstructor(String.class, int.class).newInstance(betDescriptions[response - 1], 
+                                                                         betOdds[response - 1]);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return bet;
     }
 }
