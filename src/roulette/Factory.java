31d87f5e0e3ab6b6bd187a5b541e66c28b5ba5e5
Alan Cavalcanti <alankmc@gmail.com>
2016-03-03 16:51:24 -0500

    Second Part: Used reflection

--- src/roulette/Factory.java
+++ src/roulette/Factory.java
@@ -1,43 +1,38 @@
 package roulette;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 
 public class Factory 
 {
 	private ArrayList<Bet> typesOfBets;
-	
+
 	private enum PossibleBets
 	{
 		RED_BLACK, ODD_EVEN, THREE_CONSECUTIVES
 	}
-	
+
 	public Factory()
 	{
 		typesOfBets = new ArrayList<Bet>();
-		
-		addBet(PossibleBets.RED_BLACK);
-		addBet(PossibleBets.ODD_EVEN);
-		addBet(PossibleBets.THREE_CONSECUTIVES);
+
+		addBet(RedBlack.class, "Red or Black", 1);
+		addBet(OddEven.class, "Odd or Even", 1);
+		addBet(ThreeConsecutive.class, "3 consecutive",11);
 	}
-	
-	private void addBet( PossibleBets betType )
+
+	private void addBet( Class betType, String description, int odds )
 	{
-		switch ( betType )
-		{
-		case RED_BLACK:
-			typesOfBets.add(new RedBlack("Red or Black", 1));
-			break;
-		case ODD_EVEN:
-			typesOfBets.add(new OddEven("Odd or Even", 1));
-			break;
-		case THREE_CONSECUTIVES:
-			typesOfBets.add(new ThreeConsecutive("Three Consecutives", 11));
-			break;
-		default:
-			break;
+		try {
+			typesOfBets.add((Bet) betType.getConstructor(String.class, int.class).newInstance(description, odds));
+		} catch (InstantiationException | IllegalAccessException
+				| IllegalArgumentException | InvocationTargetException
+				| NoSuchMethodException | SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
-	
+
 	public void askBetPrompt()
 	{
         System.out.println("You can make one of the following types of bets:");
