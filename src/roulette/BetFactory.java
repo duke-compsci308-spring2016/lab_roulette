ba8df4b8dba3e62e5ea47b6042324514f1197e9f
Krista Opsahl-Ong <kristaopsahlong@gmail.com>
2016-03-03 16:10:06 -0500

    added reflection

--- src/roulette/BetFactory.java
+++ src/roulette/BetFactory.java
@@ -1,26 +1,57 @@
 package roulette;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+import java.util.Map;
+
 import roulette.bets.OddEven;
 import roulette.bets.RedBlack;
 import roulette.bets.ThreeConsecutive;
 
 public class BetFactory {
-	private Bet[] myPossibleBets = {
-	        new RedBlack("Red or Black", 1),
-	        new OddEven("Odd or Even", 1),
-	        new ThreeConsecutive("Three in a Row", 11),
-	    };
-	
-	public Bet getBet(String betType){
-		if(betType.equalsIgnoreCase("RedBlack")){
-			return myPossibleBets[0];
+	private Map<String,Integer> keys;
+	//private Bet[] myPossibleBets = { new RedBlack("Red or Black", 1), new OddEven("Odd or Even", 1),
+			//new ThreeConsecutive("Three in a Row", 11), };
+	public BetFactory(){
+		keys.put("Red or Black",1);
+		keys.put("Odd or Even",1);
+		keys.put("Three in a Row",11);
+
+	}
+	public Bet getBet(String betType) {
+		Constructor<Bet> myConstructor = null;
+		Bet myInstance = null;
+		try {
+			try {
+				myConstructor = (Constructor<Bet>) Class.forName("roulette.bets"+betType).getConstructor(String.class,Integer.class);
+			} catch (NoSuchMethodException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SecurityException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			myConstructor.setAccessible(true);
+			try {
+				myInstance = myConstructor.newInstance(betType, keys.get(betType));
+			} catch (IllegalArgumentException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InvocationTargetException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		else if(betType.equalsIgnoreCase("OddEven")){
-			return myPossibleBets[1];
-		}
-		else if(betType.equalsIgnoreCase("ThreeConsecutive")){
-			return myPossibleBets[2];
-		}
-		return null;
+		return myInstance;
 	}
 }
