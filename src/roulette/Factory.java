2b4192d3e5a5fab94371425042e5136c83aaa620
Raghav <raghav.kedia@duke.edu>
2016-03-03 16:20:02 -0500

    created properties file for bets

--- src/roulette/Factory.java
+++ src/roulette/Factory.java
@@ -1,7 +1,9 @@
 package roulette;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ResourceBundle;
 
 import roulette.bets.OddEven;
 import roulette.bets.RedBlack;
@@ -11,14 +13,12 @@ import util.ConsoleReader;
 public class Factory {
 
 	List<Bet> myBets;
+	private static ResourceBundle myResourceBundle = ResourceBundle.getBundle("roulette/Bets");
 	
 	public Factory() {
 		// TODO Auto-generated constructor stub
 		myBets = new ArrayList<Bet>();
-		
-		myBets.add( new OddEven("Odd or Even", 1));
-		myBets.add( new RedBlack("Red or Black", 1));
-		myBets.add( new ThreeConsecutive("Three in a Row", 11));
+
 	}
 	
 	public static Bet createBet(String type) {
@@ -27,7 +27,8 @@ public class Factory {
 			Bet bet;
 			try {
 				c = Class.forName("roulette."+ type );
-				bet = (Bet)c.newInstance();
+				String[] args = myResourceBundle.getString(type).split(",");
+				bet = (Bet)c.getConstructor(String.class, Integer.class).newInstance(args[0], Integer.parseInt(args[1]));
 				return bet;
 			} catch (ClassNotFoundException e) {
 				// TODO Auto-generated catch block
@@ -36,6 +37,10 @@ public class Factory {
 				e.printStackTrace();
 			} catch (InstantiationException e) {
 				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch(InvocationTargetException e){
+				e.printStackTrace();
+			} catch(NoSuchMethodException e){
 				e.printStackTrace();
 			}
 			
@@ -50,7 +55,7 @@ public class Factory {
     private Bet promptForBet () {
         System.out.println("You can make one of the following types of bets:");
         for (int k = 0; k < myBets.size(); k++) {
-            System.out.println(String.format("%d) %s", (k + 1), myBets.get(k)));
+            System.out.println(myResourceBundle.getKeys());
         }
         int response = ConsoleReader.promptRange("Please make a choice", 1, myBets.size());
         return myBets.get(response - 1);
