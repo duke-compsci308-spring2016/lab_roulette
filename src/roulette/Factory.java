f177fc83c5e5cae6d4751867280433035f7539cd
Sophie Guo <yg37@duke.edu>
2016-03-03 16:05:54 -0500

    finished step

--- src/roulette/Factory.java
+++ src/roulette/Factory.java
@@ -1,22 +1,13 @@
 package roulette;
 
+import java.lang.reflect.InvocationTargetException;
+
 import util.ConsoleReader;
 
 public class Factory {
-	private Bet[] subclasses = { new RedBlack("Red or Black", 1), new OddEven("Odd or Even", 1),
-			new ThreeConsecutive("Three in a Row", 11), };
-
-	public Bet getBet() {
-		System.out.println("You can make one of the following types of bets:");
-		for (int k = 0; k < subclasses.length; k++) {
-			System.out.println(String.format("%d) %s", (k + 1), subclasses[k].getDescription()));
-		}
-		int response = ConsoleReader.promptRange("Please make a choice", 1, subclasses.length);
-		if (response < 1 || response > subclasses.length) {
-			return subclasses[response - 1];
-		} else {
-			throw new ArrayIndexOutOfBoundsException("Please select a valid bet");
-		}
+	public static Bet getBet(String betType, String description, int odds) throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException{
+		Class<?> betClass = Class.forName(betType);
+		Bet bet = (Bet) betClass.getConstructor().newInstance(description,odds);
+		return bet;
 	}
-
 }
