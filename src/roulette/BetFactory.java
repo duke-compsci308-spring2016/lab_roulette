992a5bc6830a11f1ba224521be26182bed92ec11
Jon Im <imisjonathan@gmail.com>
2016-03-03 16:24:28 -0500

    Use reflection to instantiate bet

--- src/roulette/BetFactory.java
+++ src/roulette/BetFactory.java
@@ -3,17 +3,33 @@ package roulette;
 import java.util.Arrays;
 import java.util.List;
 import util.ConsoleReader;
+import java.lang.reflect.*;
 
 public class BetFactory {
     
-    private List<Bet> myPossibleBets = Arrays.asList(new Bet[]{
-        new RedBlack("Red or Black", 1),
-        new OddEven("Odd or Even", 1),
-        new ThreeConsecutive("Three in a Row", 11),
+    private List<String> myPossibleBets = Arrays.asList(new String[]{
+                                                                     "RedBlack",
+                                                                     "OddEven",
+                                                                     "ThreeConsecutive"
+//        new RedBlack("Red or Black", 1),
+//        new OddEven("Odd or Even", 1),
+//        new ThreeConsecutive("Three in a Row", 11),
     });
     
     public Bet getBetInstance(int betNumber){
-        return myPossibleBets.get(betNumber);
+        Class<?> c;
+        try {
+            c = Class.forName("roulette." + myPossibleBets.get(betNumber-1));
+            Constructor construct = c.getConstructor(String.class, Integer.TYPE);//getConstructor(String.class, Integer.TYPE);
+            return (Bet) construct.newInstance(myPossibleBets.get(betNumber-1), 1);
+        }
+        catch (ReflectiveOperationException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return null;
+
+      
     }
     
     /**
@@ -22,7 +38,7 @@ public class BetFactory {
     public void printOptions () {
         System.out.println("You can make one of the following types of bets:");
         for (int k = 0; k < myPossibleBets.size(); k++) {
-            System.out.println(String.format("%d) %s", (k + 1), myPossibleBets.get(k).getDescription()));
+            System.out.println(String.format("%d) %s", (k + 1), this.getBetInstance(k).getDescription()));
         }
     }
     
