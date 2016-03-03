60e52aa01d76b5f19654d27f4f2401aae94290b7
Melissa Zhang <mfz2@duke.edu>
2016-03-03 16:09:29 -0500

    properties file

--- src/roulette/BetFactory.java
+++ src/roulette/BetFactory.java
@@ -1,10 +1,14 @@
 package roulette;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.ResourceBundle;
 
 public class BetFactory {
 	List<Bet> betClassList = Arrays.asList(new OddEven(null, 0),new RedBlack(null, 0),new ThreeConsecutive(null, 0));
+	ResourceBundle betResources = ResourceBundle.getBundle("betProperties");
 	public BetFactory(){
 		
 	}
@@ -18,7 +22,26 @@ public class BetFactory {
 		}
 		
 		try {
-			return (Bet) c.newInstance();
+			Constructor betConstructor = null;
+			try {
+				betConstructor = c.getConstructor();
+			} catch (NoSuchMethodException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (SecurityException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			
+			try {
+				return (Bet) betConstructor.newInstance(betResources.getString(name+"Odds"), betResources.getString(name+"Description"));
+			} catch (IllegalArgumentException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InvocationTargetException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		} catch (InstantiationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
