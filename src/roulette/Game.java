8140141eb6b14b05096aa6c67c85c29e4f505554
Cambidge Shae Van Wagoner <csv5@duke.edu>
2016-03-03 15:42:29 -0500

    factory first step

--- src/roulette/Game.java
+++ src/roulette/Game.java
@@ -15,11 +15,8 @@ public class Game {
     // name of the game
     private static final String DEFAULT_NAME = "Roulette";
     // add new bet subclasses here
-    private Bet[] myPossibleBets = {
-        new RedBlack("Red or Black", 1),
-        new OddEven("Odd or Even", 1),
-        new ThreeConsecutive("Three in a Row", 11),
-    };
+    private BetFactory betFactory;
+ 
     private Wheel myWheel;
 
     /**
@@ -68,11 +65,6 @@ public class Game {
      * Prompt the user to make a bet from a menu of choices.
      */
     private Bet promptForBet () {
-        System.out.println("You can make one of the following types of bets:");
-        for (int k = 0; k < myPossibleBets.length; k++) {
-            System.out.println(String.format("%d) %s", (k + 1), myPossibleBets[k]));
-        }
-        int response = ConsoleReader.promptRange("Please make a choice", 1, myPossibleBets.length);
-        return myPossibleBets[response - 1];
+        return betFactory.getBet();
     }
 }
